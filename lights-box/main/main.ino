238b56d4a55abdfeb38e167d2b6eeeabfd83b1ad
bdronneau <basti1.dr@gmail.com>
2023-01-17 15:03:51 +0100

    chore: clean code

--- lights-box/main/main.ino
+++ lights-box/main/main.ino
@@ -1,10 +1,10 @@
 #include "pitches.h"
 
 // constants won't change. They're used here to set pin numbers:
-const int ledPin4 =  2;
-const int ledPin3 =  3;
-const int ledPin1 =  4;
-const int ledPin2 =  5;
+const int ledPin4 =  5;
+const int ledPin3 =  4;
+const int ledPin1 =  2;
+const int ledPin2 =  3;
 const int ledPin5 =  6;
 const int ledPin6 =  7;
 const int ledPin7 =  8;
@@ -12,8 +12,6 @@ const int ledPin8 =  9;
 
 const int buttonPin1 = A0;
 const int buttonPin2 = A1;
-const int buttonPin3 = A2;
-//const int buttonPin4 = A3;
 
 const int buzzerPin = A5;
 
@@ -35,7 +33,7 @@ void setup() {
 
   // initialize the BUZZER as an outout:
   pinMode(buzzerPin, OUTPUT);
-  
+
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
@@ -49,24 +47,23 @@ void beep(int note, int duration)
 {
   //Play tone on buzzerPin
   tone(buzzerPin, note, duration);
-  
-  //Play different LED depending on value of 'counter'
+
   if(counter % 2 == 0) {
-    //digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin1, HIGH);
     delay(duration);
-    //digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin1, LOW);
   }
   else {
-    //digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin8, HIGH);
     delay(duration);
-    //digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin8, LOW);
   }
-  
+
   //Stop tone on buzzerPin
   noTone(buzzerPin);
-  
+
   delay(50);
-  
+
   //Increment counter
   counter++;
 }
@@ -76,37 +73,33 @@ void loop() {
   int buttonPin1State = digitalRead(buttonPin1);
   int buttonPin2State = digitalRead(buttonPin2);
   int buttonPin3State = digitalRead(buttonPin3);
-  //int buttonPin4State = digitalRead(buttonPin4);
 
-  Serial.println(buttonPin1State);
-  Serial.println(buttonPin2State);
+  Serial.print("Button1: ");
+  Serial.print(buttonPin1State);
+  Serial.print(" / Button2: ");
+  Serial.print(buttonPin2State);
+  Serial.print(" / Button3: ");
   Serial.println(buttonPin3State);
+  Serial.print("State:");
+  Serial.println(stateButton);
   if (buttonPin1State == LOW) {
     stateButton = 1;
   } else if (buttonPin2State == LOW) {
     if (stateButton == 2) {
-      stateButton = 0; 
+      stateButton = 0;
     } else {
-      stateButton = 2; 
+      stateButton = 2;
     }
-  }
-  else if (buttonPin3State == LOW) {
+  } else if (buttonPin3State == LOW) {
     if (stateButton == 3) {
-      stateButton = 0; 
+      stateButton = 0;
     } else {
-      stateButton = 3; 
+      stateButton = 3;
     }
   }
-//  } else if (buttonPin4State == HIGH) {
-//    stateButton = 4;
-//  }
 
   if (stateButton == 1) {
     Serial.println(" 'Song'");
-//    if (stateSong == 0)
-//    {
-//      singMario(1); 
-//    }
     if (stateSong == 1) {
       singMario(2);
     }
@@ -127,94 +120,109 @@ void loop() {
     } else {
       stateSong++;
     }
-    
+
     stateButton = 0;
   }
 
-  
+
   if (stateButton == 0) {
-//    Serial.println(" 'Serpent LED'");
+    Serial.println(" 'Serpent LED'");
     digitalWrite(ledPin1, HIGH);
+
     delay(100);
+
     digitalWrite(ledPin1, LOW);
     digitalWrite(ledPin2, HIGH);
+
     delay(100);
+
     digitalWrite(ledPin2, LOW);
     digitalWrite(ledPin3, HIGH);
+
     delay(100);
+
     digitalWrite(ledPin3, LOW);
     digitalWrite(ledPin4, HIGH);
+
     delay(100);
+
     digitalWrite(ledPin4, LOW);
     digitalWrite(ledPin5, HIGH);
+
     delay(100);
+
     digitalWrite(ledPin5, LOW);
     digitalWrite(ledPin6, HIGH);
+
     delay(100);
+
     digitalWrite(ledPin6, LOW);
     digitalWrite(ledPin7, HIGH);
+
     delay(100);
+
     digitalWrite(ledPin7, LOW);
     digitalWrite(ledPin8, HIGH);
+
     delay(100);
+
     digitalWrite(ledPin8, LOW);
   }
-  
+
   if (stateButton == 2) {
     Serial.println(" 'Blink LED'");
     digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin3, HIGH);
     digitalWrite(ledPin5, HIGH);
     digitalWrite(ledPin7, HIGH);
-    
+
     delay(300);
-    
+
     digitalWrite(ledPin1, LOW);
     digitalWrite(ledPin3, LOW);
     digitalWrite(ledPin5, LOW);
     digitalWrite(ledPin7, LOW);
-    
+
     digitalWrite(ledPin2, HIGH);
     digitalWrite(ledPin4, HIGH);
     digitalWrite(ledPin6, HIGH);
     digitalWrite(ledPin8, HIGH);
-    
-    delay(300);
 
+    delay(300);
     digitalWrite(ledPin2, LOW);
     digitalWrite(ledPin4, LOW);
     digitalWrite(ledPin6, LOW);
     digitalWrite(ledPin8, LOW);
   }
-  
+
   if (stateButton == 3) {
     Serial.println(" 'Duo Blink LED'");
     digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin2, HIGH);
-    
+
     delay(300);
-    
+
     digitalWrite(ledPin1, LOW);
     digitalWrite(ledPin2, LOW);
     digitalWrite(ledPin5, HIGH);
     digitalWrite(ledPin6, HIGH);
-    
+
     delay(300);
-    
+
     digitalWrite(ledPin5, LOW);
     digitalWrite(ledPin6, LOW);
     digitalWrite(ledPin3, HIGH);
     digitalWrite(ledPin4, HIGH);
 
     delay(300);
-    
+
     digitalWrite(ledPin3, LOW);
     digitalWrite(ledPin4, LOW);
     digitalWrite(ledPin7, HIGH);
     digitalWrite(ledPin8, HIGH);
-    
+
     delay(300);
-    
+
     digitalWrite(ledPin7, LOW);
     digitalWrite(ledPin8, LOW);
   }
